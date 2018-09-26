17a27e73ce5a9183dd65cb04db2a34b93a6ead88
Nicco_E <nengbeck@csumb.edu>
2018-09-25 19:48:26 -0700

    FirstUpload
    Sidebar not working, user$.name does not "exist"

---
+++ ng6 copy/src/app/details/details.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.scss']
+})
+
+export class DetailsComponent implements OnInit {
+
+  user$: Object;
+
+  constructor(private route: ActivatedRoute, private data: DataService) {
+     this.route.params.subscribe( params => console.log(params.id ));
+     console.log('hi');
+  }
+                                          // this.user$ =
+
+  ngOnInit() {
+   /* this.data.getUser(this.user$).subscribe(
+      data => {this.user$ = data; console.log(data);
+      }); */
+  }
+
+}
